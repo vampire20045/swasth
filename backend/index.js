ac004d2e872bfbce58f5a1b45996edd8615b5fc8
aryan <aryangupta98711@gamil.com>
2024-09-17 17:15:25 +0530

    fifth

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const bodyParser =require('body-parser');
-const mongoose =require('moongoose');
+const mongoose =require('mongoose');
 const cors= require('cors');
 const app= express();
 app.use(express.json());
