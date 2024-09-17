36dd3c26c83812a26ae2a561aec84279002ff8c6
aryan <aryangupta98711@gamil.com>
2024-09-17 17:14:50 +0530

    fifth

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,30 +1,68 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './css/login.css';
+import image from '../assets/image1.jpg'
 
-function Login(){
- return(
-    <div>
-    <center>
-        <div style ={{
-            border:"2px solid black",
-            height:"600px",
-            width:"600px"}}>
-            <h1> Login </h1>
-        <TextField id="filled-basic" label="Username" variant="filled" />
-        <br></br>
-         <TextField id="filled-basic" label="Password" variant="filled" type="password" />
-         <br></br>
-            <Button variant="contained" style={{marginTop:"20px"}}>Login</Button>
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
 
-         
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Login form submitted', formData);
+    };
 
+    return (
+        <div className="container">
+            {/* Left Side: Login Form */}
+            <div className="content-box">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-box">
+                        <input
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label>Email</label>
+                    </div>
+                    <div className="input-box">
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label>Password</label>
+                    </div>
+                    <div className="forgot-password">
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                    <button type="submit" className="btn">Login</button>
+                </form>
+                <div className="link-box">
+                    <p>Don't have an account? <Link to="/Signup">Sign up</Link></p>
+                </div>
+            </div>
 
-            
+            <div className="image-box">
+                <img src={image} alt="Login" />
+            </div>
         </div>
-        </center>
-    </div>
- )
-}
+    );
+};
+
 export default Login;
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,30 +1,87 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './css/login.css';
+import image from '../assets/image1.jpg'
+const Signup = () => {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-function Signup(){
- return(
-    <div>
-    <center>
-        <div style ={{
-            border:"2px solid black",
-            height:"600px",
-            width:"600px"}}>
-            <h1> Signup</h1>
-        <TextField id="filled-basic" label="Username" variant="filled" />
-        <br></br>
-         <TextField id="filled-basic" label="Password" variant="filled" type="password" />
-         <br></br>
-            <Button variant="contained" style={{marginTop:"20px"}}>Signup</Button>
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Sign-up form submitted', formData);
+    };
 
-         
+    return (
+        <div className="container">
+            {/* Left Side: Sign-Up Form */}
+            <div className="content-box">
+                <h2>Sign Up</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-box">
+                        <input
+                            type="text"
+                            name="fullName"
+                            value={formData.fullName}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label>Full Name</label>
+                    </div>
+                    <div className="input-box">
+                        <input
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label>Email</label>
+                    </div>
+                    <div className="input-box">
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label>Password</label>
+                    </div>
+                    <div className="input-box">
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label>Confirm Password</label>
+                    </div>
+                    <button type="submit" className="btn">Sign Up</button>
+                </form>
+                <div className="link-box">
+                    <p>Already have an account? <Link to="/Login">Login</Link></p>
+                </div>
+            </div>
 
-
-            
+            {/* Right Side: Image */}
+            <div className="image-box">
+                <img src={image} alt="Sign Up" />
+            </div>
         </div>
-        </center>
-    </div>
- )
-}
+    );
+};
+
 export default Signup;
